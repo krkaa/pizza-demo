492c2dd773873a4b19692e59ad987758c1434b3e
krkaa <buubzpavel@gmail.com>
2020-03-24 12:43:56 +0300

    Updates

---
+++ precache-manifest.730724e137b75590d7e6c8221fb75894.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ea76ee66c764372fa47f4b87374b11c",
+    "url": "/pizza-demo/index.html"
+  },
+  {
+    "revision": "b59c7a1e2bdad42ee8b4",
+    "url": "/pizza-demo/static/css/main.8aa595e5.chunk.css"
+  },
+  {
+    "revision": "dc658155c87f2b9b9739",
+    "url": "/pizza-demo/static/js/2.bcef0e7c.chunk.js"
+  },
+  {
+    "revision": "170392c3acad6bbffec743442b1d0ac3",
+    "url": "/pizza-demo/static/js/2.bcef0e7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b59c7a1e2bdad42ee8b4",
+    "url": "/pizza-demo/static/js/main.fcd55983.chunk.js"
+  },
+  {
+    "revision": "69d87c5a39ae98c49243",
+    "url": "/pizza-demo/static/js/runtime-main.d3298116.js"
+  },
+  {
+    "revision": "4df8904ba1b1516da15396dadde97a37",
+    "url": "/pizza-demo/static/media/background-image.4df8904b.jpg"
+  },
+  {
+    "revision": "99420fc7eee8276802082ca16fc16917",
+    "url": "/pizza-demo/static/media/home-image.99420fc7.jpg"
+  },
+  {
+    "revision": "991d78ee844f43e87488d0c2765f4fb5",
+    "url": "/pizza-demo/static/media/logo.991d78ee.png"
+  },
+  {
+    "revision": "b303b41936e0909ebb18839593e5d9aa",
+    "url": "/pizza-demo/static/media/preloader.b303b419.svg"
+  }
+]);
